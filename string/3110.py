e726fae7528e930888281956f3afd72a6929aac2
Uakhap Yernur <cors1ka.004@gmail.com>
2025-02-15 22:45:34 +0500

    commets to the problem

--- string/3110.py
+++ string/3110.py
@@ -1,5 +1,11 @@
 class Solution:
     def scoreOfString(self, s: str) -> int:
+        """
+        You are given a string s. The score of a string
+        is defined as the sum of the absolute difference 
+        between the ASCII values of adjacent characters.
+        Return the score of s.
+        """
         x = 0
         for i in range(0,len(s)-1):
             x += abs(ord(s[i]) - ord(s[i+1]))
